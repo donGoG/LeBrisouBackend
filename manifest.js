9127812bbb10c527b47d9fb3bc6ec559e5493744
amaurybrisou <amaury.brisou@gmail.com>
2014-07-19 18:27:16 +0200

    manifest

--- manifest.js
+++ manifest.js
@@ -1,42 +1,48 @@
 var fs = require('fs')
 
-domain = 'puzzledge.eu'
+domain = 'localhost'
 
 module.exports = {
-	"pack" : {
-		'cache': 'catbox-memory'
+	pack : {
+		cache: {
+			engine: require('catbox-memory')
+			// engine: require('catbox-redis'),
+			// partition: 'LeBrisouBackend',
+			// host: '192.168.1.14',
+			// port: 6379
+		}
 	},
-	"servers" : [
+	servers : [
 		{
-			"port" : 8081,
-			// "host" : "dic.local",
-			"options": {
-				"tls": {
-				  'key': fs.readFileSync( __dirname + '/plugins/dictionary-api/cert/'+domain+'.key'),
-				  'cert': fs.readFileSync(__dirname + '/plugins/dictionary-api/cert/'+domain+'.crt')
-				},
-	      "labels": "dictionary-api",
-	      "cors": true,
-	      "debug": {
-	      	'request' : ['error', 'uncaught', 'log']
+			port : 8081,
+			// host : dic.local,
+			options: {
+				// tls: {
+				//   key: fs.readFileSync( __dirname + /plugins/dictionary-api/cert/+domain+.key),
+				//   cert: fs.readFileSync(__dirname + /plugins/dictionary-api/cert/+domain+.crt)
+				// },
+	      labels: 'dictionary-api',
+	      cors: true,
+	      debug: {
+	      	request : ['error', 'uncaught', 'log']
 	      }
 	    }
 		},
 		{
-			"port" : 8080,
-			"options": {
-	      "labels": "dictionary-web",
+			port : 8080,
+			options: {
+	      labels: 'dictionary-web',
 	    }
 		}
 	],
-	"plugins" : {
-		"../../../node_modules/good" : [
+	plugins : {
+		'../../../node_modules/good' : [
 			{
-				'options': {
-					'extendedRequests': true,
-					// 'requestsEvent': 'response',
-					'subscribers': {
-						"console" : ['request', 'log', 'error'],
+				options: {
+					extendedRequests: true,
+					// requestsEvent: response,
+					subscribers: {
+						console : ['request', 'log', 'error'],
 						'logs/requests.log':    ['request'],
 						'logs/errors.log':    ['error'],
 						'logs/log.log':    ['log'],
@@ -45,77 +51,77 @@ module.exports = {
 				}
 			}
 		],
-		"../../../node_modules/lout": { 
-			"endpoint": "/docs" 
+		'../../../node_modules/lout': { 
+			endpoint: '/docs' 
 		},
-		"../../../node_modules/tv" : {
-		  "endpoint": "/debug/console",
-		  "queryKey": "debug"
+		'../../../node_modules/tv' : {
+		  endpoint: '/debug/console',
+		  queryKey: 'debug'
 		},
-		"../../../plugins/hapi-sequelize" : [
+		'../../../plugins/hapi-sequelize' : [
 			{
-				"options" : {
-			    "dialect": "sqlite", // or 'sqlite', 'postgres', 'mariadb'
+				options : {
+			    dialect: 'sqlite', // or sqlite, postgres, mariadb
 			    //port:    5432, // or 5432 (for postgres)
-			    "storage":  __dirname + '/plugins/dictionary-rdbms/LeBrisou.sqlite',
-			    "sync": { "force" : true },
-			    "logging": console.log,
-			    "maxConcurrentQueries": 1,
-			    "native": true,
-			    "define": {
-			      "underscored": false,
-			      "freezeTableName": false,
-			      "syncOnAssociation": true,
-			      "charset": 'utf8',
-			      "collate": 'utf8_general_ci',
-			      // 'classMethods': {method1: function() {}},
-			      // 'instanceMethods': {method2: function() {}},
-			      "timestamps": true
+			    storage:  __dirname + '/plugins/dictionary-rdbms/LeBrisou.sqlite',
+			    sync: { force : true },
+			    logging: console.log,
+			    maxConcurrentQueries: 1,
+			    native: true,
+			    define: {
+			      underscored: false,
+			      freezeTableName: false,
+			      syncOnAssociation: true,
+			      charset: 'utf8',
+			      collate: 'utf8_general_ci',
+			      // classMethods: {method1: function() {}},
+			      // instanceMethods: {method2: function() {}},
+			      timestamps: true
 			    },
-			    "pool": { "maxConnections": 1, "maxIdleTime": 30}, //currently useless for sqlite maybe in another version
+			    pool: { maxConnections: 1, maxIdleTime: 30}, //currently useless for sqlite maybe in another version
 				},
-				"select" : ["dictionary-api"]
+				select : ['dictionary-api']
 			}
 		],
-		"../../../plugins/dictionary-rdbms" : [
+		'../../../plugins/dictionary-rdbms' : [
 			{
-				"options" : {
-					"drop": false,
-					'sync': {'force': true }
+				options : {
+					drop: false,
+					sync: {force: true }
 				},
-				"select" : ["dictionary-api"]
+				select : ['dictionary-api']
 			}
 		],
-		"../../../plugins/dictionary-api" : [
+		'../../../plugins/dictionary-api' : [
 			{
-				"select" : ["dictionary-api"],
-				"route": {
-					"prefix" : "/api",
-					// "vhost" : "dic.local"
+				select : ['dictionary-api'],
+				route: {
+					prefix : '/api',
+					//vhost : dic.local
 				}
 			}
 		],
-		"../../../plugins/dictionary-web" : [
+		'../../../plugins/dictionary-web' : [
 			{
-				'options': {
-					'sitename': 'LeBrisouBackend',
-					'author': 'Amaury Brisou'
+				options: {
+					sitename: 'LeBrisouBackend',
+					author: 'Amaury Brisou'
 				},
-				'select': ['dictionary-web']
+				select: ['dictionary-web']
 			}
 		],
-		"../../../plugins/dictionary-parser" : [
+		'../../../plugins/dictionary-parser' : [
 			{
-				'options' : {
-					'inject': false,
-					'sheets' : ['AB', 'C', 'D'],
-					'input' :  __dirname + '/assets/input/dictionary_database.xlsx',
-					'output' : __dirname + '/assets/output/excel_dump.json'
+				options : {
+					inject: false,
+					sheets : ['AB', 'C', 'D'],
+					input :  __dirname + '/assets/input/dictionary_database.xlsx',
+					output : __dirname + '/assets/output/excel_dump.json'
 				},
-				"select" : ["dictionary-api"]
+				select : ['dictionary-api']
 			}
 		],
-		"../../../plugins/dictionary-error" : [
+		'../../../plugins/dictionary-error' : [
 			{
 			}
 		],
